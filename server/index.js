6ce8b6b5c050ef329fdfcba965dc1b39bc8bec6b
JENNIFER GONZALEZ ALZATE <jgonzal10@gmail.com>
2019-06-05 00:31:13 -0300

    architecture changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/dist/stars'));
 
 if (process.env.NODE_ENV === 'production') {
     console.log('prod')
-    app.use(express.static(path.join(__dirname, 'stars/dist/index.html')));
+    app.use(express.static(path.join(__dirname, '/stars/dist/index.html')));
 }
 
 app.use('/api', routes)
